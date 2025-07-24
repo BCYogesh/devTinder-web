d7dfad333bc206ef1246ab461379aac83050a431
BCYogesh <bcyogesh4961@gmail.com>
2025-07-25 00:47:02 +0530

    socket cors issue

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,7 +2,10 @@ import { io } from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 const createSocketConnection = () => {
-    return io(BASE_URL);
+    return io(BASE_URL, {
+        withCredentials: true,
+        transports: ['websocket'],
+    });
 }
 
 export default createSocketConnection;
