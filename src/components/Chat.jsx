e4b0e9a8a0a2ba133a64c37979faa091f9272139
BCYogesh <bcyogesh4961@gmail.com>
2025-07-24 21:48:41 +0530

    Chat UI implementation done

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,15 +1,52 @@
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import createSocketConnection from "../utils/socket";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { extractISTTime } from "../utils/constants";
+import { addConnections } from "../utils/connectionSlice";
 
 const Chat = () => {
   const { targetUserId } = useParams();
   const user = useSelector((store) => store.user?.data);
+  const connections = useSelector((store) => store.connections);
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
+  const scrollRef = useRef(null);
+  const dispatch = useDispatch();
 
   const userId = user?._id;
+
+  const fetchMessages = async () => {
+    const chat = await axios.get(BASE_URL + "/chat/" + targetUserId, {
+      withCredentials: true,
+    });
+
+    const chatMessages = chat?.data?.data?.messages?.map((msg) => {
+      const time = extractISTTime(msg?.createdAt);
+
+      const { senderId, text } = msg;
+      return {
+        firstName: senderId?.firstName,
+        lastName: senderId?.lastName,
+        createdAt: time,
+        text,
+      };
+    });
+    setMessages(chatMessages);
+  };
+
+  const getConnections = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/connections", {
+        withCredentials: true,
+      });
+      dispatch(addConnections(res?.data?.data));
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   useEffect(() => {
     if (!userId) {
@@ -34,6 +71,16 @@ const Chat = () => {
     };
   }, [userId, targetUserId]);
 
+  useEffect(() => {
+    if (targetUserId) {
+      fetchMessages();
+    }
+  }, [targetUserId]);
+
+  useEffect(() => {
+    getConnections();
+  }, []);
+
   const sendMessage = () => {
     const socket = createSocketConnection();
 
@@ -46,33 +93,50 @@ const Chat = () => {
       text: newMessage,
     });
     setNewMessage("");
+
+    setTimeout(() => {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }, 300);
   };
+
+  const chatterName = connections
+    .filter((con) => con._id == targetUserId)
+    .map((con) => con?.firstName + " " + con?.lastName);
 
   return (
     <div className="w-3/4 mx-auto h-[70vh] bg-slate-950 m-2 rounded flex flex-col">
-      <h1 className="font-bold text-xl border-b-2 border-b-gray-400 p-2">
-        Chat
+      <h1 className="font-bold text-xl border-b-2 border-b-gray-400 p-2 text-white">
+        {chatterName.join("")}
       </h1>
-      <div className="flex-1 overflow-y-auto p-4">
+      <div className="flex-1 overflow-y-auto p-4" ref={scrollRef}>
         {/* Messages body */}
-        {messages.map((msg, id) => (
-          <div className="chat chat-start" key={id}>
-            <div className="chat-image avatar">
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS chat bubble component"
-                  src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
-                />
+        {messages &&
+          messages.map((msg, id) => (
+            <div
+              className={
+                "chat " +
+                (user?.firstName == msg?.firstName ? "chat-end" : "chat-start")
+              }
+              key={id}
+            >
+              <div className="chat-image avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS chat bubble component"
+                    src="https://img.daisyui.com/images/profile/demo/kenobee@192.webp"
+                  />
+                </div>
+              </div>
+              <div className="chat-header text-white">
+                {msg?.firstName + " " + msg?.lastName}
+                <time className="text-xs opacity-50">{msg?.createdAt}</time>
+              </div>
+              <div className="chat-bubble text-white bg-black">{msg?.text}</div>
+              <div className="chat-footer opacity-50 text-gray-100">
+                Delivered
               </div>
             </div>
-            <div className="chat-header">
-              {msg?.firstName}
-              <time className="text-xs opacity-50">12:45</time>
-            </div>
-            <div className="chat-bubble">{msg?.text}</div>
-            <div className="chat-footer opacity-50">Delivered</div>
-          </div>
-        ))}
+          ))}
       </div>
       <div className="flex items-center p-2">
         <input
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,14 @@
 export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "/api";
+
+
+export const extractISTTime = (fullDate) => {
+    const createdTime = new Date(fullDate);
+    const istTime = createdTime.toLocaleTimeString("en-IN", {
+        timeZone: "Asia/Kolkata",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+    });
+    return istTime;
+};
+
